40e2c14d27d3190c928fad2bc43746417b66d120
AdamMCraven <adam@ChannelAdam.com>
2017-02-24 17:31:23 +1000

    1.3.0 - Target .NET 4.0 instead of 4.0.3 and updated to latest ChannelAdam.Core and ChannelAdam.Soap.

--- build/pack.cmd
+++ build/pack.cmd
@@ -1,6 +1,6 @@
 SET msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
-%msbuild% ..\src\ChannelAdam.Nancy.Soap\ChannelAdam.Nancy.Soap.csproj /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.0;OutDir=bin\Release\net403
+%msbuild% ..\src\ChannelAdam.Nancy.Soap\ChannelAdam.Nancy.Soap.csproj /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v4.0;OutDir=bin\Release\net40
 
 ..\tools\nuget\nuget.exe pack ..\src\ChannelAdam.Nancy.Soap\ChannelAdam.Nancy.Soap.nuspec -Symbols
 
